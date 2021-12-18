c32b1b6d5c7489a2d4bce2758ca96842998edd3e
ivaluisnazare <ivaluisnazare@gmail.com>
2021-12-18 08:01:54 -0300

    add json convertion utils

--- src/test/java/bearapidio/example/bearapi/controller/BeerControllerTest.java
+++ src/test/java/bearapidio/example/bearapi/controller/BeerControllerTest.java
@@ -10,14 +10,16 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
-
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
-
+import static bearapidio.example.bearapi.utils.JsonConvertionUtils.asJsonString;
+import static org.hamcrest.core.Is.is;
 import static org.mockito.Mockito.when;
-
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -27,7 +29,7 @@ public class BeerControllerTest {
     private static final long VALID_BEER_ID = 1L;
     private static final long INVALID_BEER_ID = 2l;
     private static final String BEER_API_SUBPATH_INCREMENT_URL = "/increment";
-    private static final String BEER_API_SUBPATH_DECREMENT_URL = "/decrement";
+//    private static final String BEER_API_SUBPATH_DECREMENT_URL = "/decrement";
 
     private MockMvc mockMvc;
 
@@ -54,6 +56,13 @@ public class BeerControllerTest {
         when(beerService.createBeer(beerDTO)).thenReturn(beerDTO);
 
         // then
+        mockMvc.perform(post(BEER_API_URL_PATH)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(beerDTO)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.name", is(beerDTO.getName())))
+                .andExpect(jsonPath("$.brand", is(beerDTO.getBrand())))
+                .andExpect(jsonPath("$.type", is(beerDTO.getType().toString())));
 
     }
 }
